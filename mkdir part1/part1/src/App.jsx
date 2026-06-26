5c29381dc8771519a7f2cb3f88c990f891a6fcdb
unknown <flarx3@gmail.com>
2026-06-26 11:42:06 -0400

    avance con  la destructuracion 1.c

--- mkdir part1/part1/src/App.jsx
+++ mkdir part1/part1/src/App.jsx
@@ -1,7 +1,26 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const App = () => {
+  const [ counter, setCounter ] = useState(1)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  
+  console.log('rendering...', counter)
+
+
+  
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
+    </div>
   )
 }
 
