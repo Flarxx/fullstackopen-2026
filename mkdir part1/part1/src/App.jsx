6910a3513bdd35f2db3a2ee8a4e9bceec632f854
unknown <flarx3@gmail.com>
2026-06-30 14:13:42 -0400

    final de la parte 1.c

--- mkdir part1/part1/src/App.jsx
+++ mkdir part1/part1/src/App.jsx
@@ -1,25 +1,38 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
 const App = () => {
-  const [ counter, setCounter ] = useState(1)
+  const [counter, setCounter] = useState(0)
 
-  const handleClick = () => {
-    console.log('clicked')
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
   }
-  
-  console.log('rendering...', counter)
 
+  const decreaseByOne = () => { 
 
-  
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button onSmash={increaseByOne} text="plus" />
+      <Button onSmash={setToZero} text="zero" />
+      <Button onSmash={decreaseByOne} text="minus" />
     </div>
   )
 }
